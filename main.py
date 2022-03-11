616e6cdfb611001497818f1db4fd87b8cff14416
Trains77 <trains77videos123@gmail.com>
2022-03-10 20:42:03 -0500

    moved ls to its own function

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Files =  os.listdir()
 ARGS = sys.argv
 Arg_Path = 0
 Use_Arg_Path = False
+def run_ls():
+    os.system("ls --color=auto " + str(ARGS[1]) + " " + str(ARGS[2]) + " " + str(ARGS[3]) + " " + str(ARGS[4]) + " " + str(ARGS[5]) + " " + str(ARGS[6]) + " " + str(ARGS[7]) + " " + str(ARGS[8]) + " " + str(ARGS[9]) + " " + str(ARGS[10]))
 for i in range(10):
     ARGS.append('')
 for i in range(9):
@@ -19,11 +21,11 @@ for i in range(9):
 #starts_with_slash = e.startswith("/")
 if Use_Arg_Path  == False:
     if not os.path.exists(".hide"):
-        os.system("ls --color=auto " + str(ARGS[1]) + " " + str(ARGS[2]) + " " + str(ARGS[3]) + " " + str(ARGS[4]) + " " + str(ARGS[5]) + " " + str(ARGS[6]) + " " + str(ARGS[7]) + " " + str(ARGS[8]) + " " + str(ARGS[9]) + " " + str(ARGS[10]))
+        run_ls()
     else:
         print("ls: cannot open directory '" + str(os.getcwd()) +  "': Permission denied")
 else:
     if not os.path.exists(str(ARGS[Arg_Path]) + "/.hide"):
-        os.system("ls --color=auto " + str(ARGS[1]) + " " + str(ARGS[2]) + " " + str(ARGS[3]) + " " + str(ARGS[4]) + " " + str(ARGS[5]) + " " + str(ARGS[6]) + " " + str(ARGS[7]) + " " + str(ARGS[8]) + " " + str(ARGS[9]) + " " + str(ARGS[10]))
+        run_ls()
     else:
         print("ls: cannot open directory '" + str(ARGS[Arg_Path]) +  "': Permission denied")
